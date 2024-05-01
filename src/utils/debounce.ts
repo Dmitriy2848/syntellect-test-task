44905ef921588e6e2736d48902ec021263300298
Dmitriy2848 <Dmitriy1747@yandex.ru>
2024-05-01 20:55:20 +0300

    feat(v0.0.11): add debounce function

---
+++ src/utils/debounce.ts
@@ -0,0 +1,11 @@
+export const debounce = (func: (...args: any[]) => void, wait = 200) => {
+  let timeout: NodeJS.Timeout;
+  return function executedFunction(...args: any[]) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+};
